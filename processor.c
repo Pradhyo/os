00e6d1e15df01120bc1c513a2cf5367001bece38
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-19 12:58:46 +0530

    Add files from before, for processor and receiver

---
+++ processor.c
@@ -0,0 +1,40 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+#include <string.h>
+
+main()
+{
+    int shmid;
+    key_t key;
+    char *shm, *s;
+    FILE *fptr;
+
+	fptr=fopen("digits.out","w");    //Open file initially in write mode so that previous contents are removed
+    key = 888; // Random key 888 to identify the shared memory location
+    shmid = shmget(key, 27, 0666);  //locate the segment
+    shm = shmat(shmid, NULL, 0);   //attach segment to data space
+    while(1)//infinite loop
+    {
+    fptr=fopen("digits.out","a"); //Open the file to append new lines each time a line is entered   
+    int count=0; //Keeps track of the number of digits
+    char copy[300];
+    for (s = shm; *s !='\0'; s++) //Points to each character in the string from start until null  
+        {
+	if (*s=='\n') //As we don't want a new line between the line and the digit count
+	fprintf(fptr,"  --->"); 
+	else	
+	putc(*s,fptr); //Print current character onto the output file
+	if (*s=='1'||*s=='2'||*s=='3'||*s=='4'||*s=='5'||*s=='6'||*s=='7'||*s=='8'||*s=='9'||*s=='0')
+	{count++;}  //Increment when it is a digit
+	}
+	fprintf(fptr,"%d\n",count); //Print count value next to the line in the file
+	strcpy(copy,shm); //Copying current value to temporary location local to this program
+   	fclose(fptr); //Close the file
+	point1:
+    if (strcmp(copy,shm)==0)  //To do nothing as long as a new line is not entered (as long as the value at shared memory location is unchanged
+        goto point1;
+    }
+ 
+}
---
+++ receiver.c
@@ -0,0 +1,24 @@
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdio.h>
+
+
+main()
+{
+
+    char c;
+    int shmid;
+    key_t key;
+    char *shm;
+    key = 888; // Random key 888
+    shmid = shmget(key, 27, IPC_CREAT | 0666);// Create the segment
+    shm = shmat(shmid, NULL, 0);//attach segment to data space
+    printf("Keep entering lines\n");
+    while(1)
+    {	
+    fgets (shm, 256, stdin); //get input from user
+    } 
+}
+
+
