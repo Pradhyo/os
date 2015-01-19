02fe22f224c033c18d0ad6850a467aff9110e1b5
Pradhyo Bijja <pradhyo@nyu.edu>
2015-01-19 14:13:52 +0530

    Make processor.c more readable

--- processor.c
+++ processor.c
@@ -11,30 +11,31 @@ main()
     char *shm, *s;
     FILE *fptr;
 
-	fptr=fopen("digits.out","w");    //Open file initially in write mode so that previous contents are removed
+	fptr=fopen("digits.out","w");    //Write mode so that previous contents are removed
     key = 888; // Random key 888 to identify the shared memory location
     shmid = shmget(key, 27, 0666);  //locate the segment
     shm = shmat(shmid, NULL, 0);   //attach segment to data space
     while(1)//infinite loop
-    {
-    fptr=fopen("digits.out","a"); //Open the file to append new lines each time a line is entered   
-    int count=0; //Keeps track of the number of digits
-    char copy[300];
-    for (s = shm; *s !='\0'; s++) //Points to each character in the string from start until null  
-        {
-	if (*s=='\n') //As we don't want a new line between the line and the digit count
-	fprintf(fptr,"  --->"); 
-	else	
-	putc(*s,fptr); //Print current character onto the output file
-	if (*s=='1'||*s=='2'||*s=='3'||*s=='4'||*s=='5'||*s=='6'||*s=='7'||*s=='8'||*s=='9'||*s=='0')
-	{count++;}  //Increment when it is a digit
-	}
-	fprintf(fptr,"%d\n",count); //Print count value next to the line in the file
-	strcpy(copy,shm); //Copying current value to temporary location local to this program
-   	fclose(fptr); //Close the file
-	point1:
-    if (strcmp(copy,shm)==0)  //To do nothing as long as a new line is not entered (as long as the value at shared memory location is unchanged
-        goto point1;
-    }
+	    {
+	    fptr=fopen("digits.out","a"); 
+	    int count=0; //Keeps track of the number of digits
+	    char copy[300]; //temporary location
+	    for (s = shm; *s !='\0'; s++) 
+		    {
+			if (*s=='\n') //We don't want a new line between the line and the digit count
+				fprintf(fptr,"  --->"); 
+			else	
+				putc(*s,fptr); //Print current character onto the output file
+			if (*s=='1'||*s=='2'||*s=='3'||*s=='4'||*s=='5'||*s=='6'||*s=='7'||*s=='8'||*s=='9'||*s=='0')
+				count++;
+			}
+		fprintf(fptr,"%d\n",count); //Print count value next to the line in the file
+		strcpy(copy,shm); 
+	   	fclose(fptr); 
+		point1:
+		//Do nothing until new line is entered (value at shared memory location is changed)
+	    if (strcmp(copy,shm)==0)  
+	        goto point1;
+	    }
  
 }
